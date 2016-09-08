a716586a3c42b6263dc0ff612420443bc792a49c
Anton Konovalov <extempl@gmail.com>
2016-09-08 17:15:11 +0300

    Add support for github ajax navigation.

--- src/script.js
+++ src/script.js
@@ -1,30 +1,36 @@
 (function () {
   "use strict"
 
-  var links = document.querySelectorAll("a[href^=github-mac]")
+  var _process = function () {
+    var links = document.querySelectorAll("a[href^=github-mac]")
 
-  if (!links.length) {
-    return
-  }
-
-  chrome.storage.sync.get(['base', 'template'], function (storage) {
-    if (!storage.base || !storage.template) {
-      return // chrome.runtime.openOptionsPage()
+    if (!links.length) {
+      return
     }
 
-    var base = storage.base
-    var template = storage.template.replace('%base%', base)
-
-    links.forEach(function (link) {
-      var href = link.getAttribute('href')
-      var matched = href.replace('%2F', '/').match(/([\w-]+)\?.*&filepath=([\w.\-\/]+)$/)
-      if (!matched || matched.length < 3) {
-        return
+    chrome.storage.sync.get(['base', 'template'], function (storage) {
+      if (!storage.base || !storage.template) {
+        return // chrome.runtime.openOptionsPage()
       }
 
-      var newHref = template.replace('%projectname%', matched[1]).replace('%filepath%', matched[2])
-      link.setAttribute('href', newHref)
+      var base = storage.base
+      var template = storage.template.replace('%base%', base)
+
+      links.forEach(function (link) {
+        var href = link.getAttribute('href')
+        var matched = href.replace('%2F', '/').match(/([\w-]+)\?.*&filepath=([\w.\-\/]+)$/)
+        if (!matched || matched.length < 3) {
+          return
+        }
+
+        var newHref = template.replace('%projectname%', matched[1]).replace('%filepath%', matched[2])
+        link.setAttribute('href', newHref)
+      })
     })
-  })
+  }
+
+  _process()
+
+  document.addEventListener("pjax:end", _process)
 
 })()
